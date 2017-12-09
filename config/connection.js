7c938220eb83070e01b9fea29792c09bfde241c7
unknown <jpek919@yahoo.com>
2017-12-08 20:35:43 -0500

    remove pwd

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'jhsP4263',
+    password: '',
     database: 'burgers_db'
   });
 };
